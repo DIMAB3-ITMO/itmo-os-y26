fa9db3cc3bfbe105f3165d12360d58a79170d413
dimon_limon <dimabelunin777@niuitmo.ru>
2024-03-07 23:30:54 +0300

    lab2: 2.sh added

---
+++ lab2/2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Вывести в файл список PID всех процессов, которые были запущены командами, расположенными в /sbin/
+sbin_output_file="2_output"
+
+ps -Ao pid,command | awk '$2 ~ /^\/sbin\// {print $1}' > "$sbin_output_file"
+# external '/' means bounds of regular expressions
